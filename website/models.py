4615fb61c2e4f70e8f5295266dbad917a0c3aa69
Gilberto Diaz <diaz.gilberto@gmail.com>
2017-05-18 13:13:07 -0500

    create Category model, ignore migrations in .gitignore, add Category to the admin page

--- website/models.py
+++ website/models.py
@@ -1,8 +1,16 @@
+"""Models for the Bangazon application."""
 from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
 class Product(models.Model):
+    """Products within the Bangazon application are represented by this model.
+
+    Author: Steve Brownlee
+    Args: Extends the models.Model Django class
+    Return: N/A
+    """
+
     seller = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
@@ -11,3 +19,23 @@ class Product(models.Model):
     description = models.TextField(blank=True, null=True)
     price = models.IntegerField()
     quantity = models.IntegerField()
+
+
+class Category(models.Model):
+    """Categories within the Bangazon application are represented by this model.
+
+    Author: Gilberto Diaz
+    Args: Extends the models.Model Django class
+    Return: N/A
+    """
+
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        """Appliying __str__ to the name feature of the Category model.
+
+        Author: Gilberto Diaz
+        Args: Extends the models.Model Django class
+        Return: A readeble representation of the object
+        """
+        return self.name
