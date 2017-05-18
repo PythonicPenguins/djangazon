6cc987db1236bee3103a3d3e7a15ba90ef29c456
Meg Ducharme <ducharme.meg@gmail.com>
2017-05-18 13:46:44 -0500

    Added order, orderproduct, and paymenttype models

--- website/models.py
+++ website/models.py
@@ -1,13 +1,46 @@
 from django.contrib.auth.models import User
 from django.db import models
+from datetime import date
 
-# Create your models here.
+
 class Product(models.Model):
     seller = models.ForeignKey(
         User,
-        on_delete=models.CASCADE,
+        on_delete=models.DO_NOTHING,
     )
     title = models.CharField(max_length=255)
     description = models.TextField(blank=True, null=True)
     price = models.IntegerField()
     quantity = models.IntegerField()
+
+
+class PaymentType(models.Model):
+    user = models.ForeignKey(
+        User,
+        on_delete = models.DO_NOTHING,
+    )
+    name = models.CharField(max_length=55)
+    account_number = models.CharField(max_length=16)
+
+
+class Order(models.Model):
+    customer = models.ForeignKey(
+        User,
+        on_delete = models.DO_NOTHING,
+    )
+    payment_type = models.ForeignKey(
+        PaymentType,
+        on_delete = models.DO_NOTHING,
+    )
+    date_created = date.today()
+
+
+class OrderProduct(models.Model):
+    order = models.ForeignKey(
+        Order,
+        on_delete = models.DO_NOTHING,
+    )
+    product = models.ForeignKey(
+        Product,
+        on_delete = models.DO_NOTHING,
+    )
